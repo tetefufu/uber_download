f397fc0e9fb64a9a52b27d0294086ea06705036c
Joel <joelbellot@hotmail.com>
2024-12-07 08:33:47 +0400

    async

--- utils/careem_client.py
+++ utils/careem_client.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiohttp
 
 from datetime import datetime, timedelta
 import logging
@@ -40,7 +42,7 @@ class CareemClient:
         return start_time_unix_millis, end_time_unix_millis, start_date_str, end_date_str
 
 
-    def get_trip_details(self, trip_id, captain_id):
+    async def get_trip_details(self, trip_id, captain_id):
         url = f"{self.captain_url}/trip-receipt/{trip_id}/en"
         self.headers = {
             "accept": "application/json, text/plain, */*",
@@ -48,24 +50,25 @@ class CareemClient:
             "authtoken": f"Bearer {self.bearer_token}",
         }
         
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-        response_data = response.json()
-        logging.info(response_data)
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                response_data = await response.json()
+                logging.info(response_data)
 
-        flat_json = {}
-        for section in response_data.get("data", {}).get("sections", []):
-            for line in section.get("lines", []):
-                left = line.get("left")
-                right = line.get("right")
-                
-                if left and right:
-                    flat_json[left] = right
+                flat_json = {}
+                for section in response_data.get("data", {}).get("sections", []):
+                    for line in section.get("lines", []):
+                        left = line.get("left")
+                        right = line.get("right")
+                        
+                        if left and right:
+                            flat_json[left] = right
 
-        return flat_json
+                return flat_json
 
 
-    def get_trips(self, captain_id):
+    async def get_trips(self, captain_id):
         url = f"{self.captain_url}/transaction/{captain_id}?cycleNumber=0&viewParams=%7B%22cycleIdx%22:%200%7D&viewType=cycle"
 
         self.headers = {
@@ -74,28 +77,29 @@ class CareemClient:
             "authtoken": f"Bearer {self.bearer_token}",
         }
         
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-        response_data = response.json()
-        logging.info(response_data)
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                response_data = await response.json()
+                logging.info(response_data)
 
-        transactions = response_data["verifiedEarningPromise"]["captainTransactions"]
+                transactions = response_data["verifiedEarningPromise"]["captainTransactions"]
 
-        result = [
-            {
-                "transactionId": transaction["transactionId"],
-                "captainName": response_data["verifiedEarningPromise"]["captainName"],
-                "captainId": response_data["verifiedEarningPromise"]["captainId"],
-                "countryName": response_data["verifiedEarningPromise"]["countryName"],
-                "uuid": transaction["uuid"]
-            }
-            for transaction in transactions
-        ]
+                result = [
+                    {
+                        "transactionId": transaction["transactionId"],
+                        "captainName": response_data["verifiedEarningPromise"]["captainName"],
+                        "captainId": response_data["verifiedEarningPromise"]["captainId"],
+                        "countryName": response_data["verifiedEarningPromise"]["countryName"],
+                        "uuid": transaction["uuid"]
+                    }
+                    for transaction in transactions
+                ]
 
-        return result
+                return result
 
 
-    def get_drivers(self):
+    async def get_drivers(self):
         start_time_unix_millis, end_time_unix_millis, start_date_str, end_date_str = self.get_dates()
 
         url = self.url + "/limo/portal/captain/acceptance/477236"
@@ -103,11 +107,13 @@ class CareemClient:
             "startTime": "1733011200000",
             "endTime": "1733356799000"
         }
-        response = requests.get(url, headers=self.headers, params=params)
-        response.raise_for_status()
-        response_data = response.json()
-        logging.info(response_data)
 
-        captain_ids = [entry['captainId'] for entry in response_data]
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(url, params=params) as response:
+                response.raise_for_status()
+                response_data = await response.json()
+                logging.info(response_data)
 
-        return captain_ids
+                captain_ids = [entry['captainId'] for entry in response_data]
+
+                return captain_ids
