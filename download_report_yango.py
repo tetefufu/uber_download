144c20f645e608c133d080847b6e3a22c4c6fc32
Joel <joelbellot@hotmail.com>
2024-12-22 09:31:36 +0400

    yango variable uuid

--- download_report_yango.py
+++ download_report_yango.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 import logging
 import os
-import time
-import aiohttp
 import asyncio
 import csv
+import uuid
 
 import requests
 
@@ -13,112 +12,10 @@ from utils.config_utils import read_config
 from utils.creds import extract_cookie_value_yango
 from urllib.parse import quote
 
+from utils.format_utils import convert_to_dict_list
 from utils.log_utils import *
+from utils.yango_client import YangoClient
 
-class YangoClient:
-    def __init__(self, cookie_str):
-        self.headers = {
-            "Cookie": cookie_str,
-            # "Accept": "application/json, text/plain, */*",
-            "Accept": "application/json",
-            "X-Client-Version": "fleet/13631",
-            "X-Park-Id": "c5cf968c4c2f4bbc947c8c0703b7e124",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "Language": "en"
-        }
-        self.base_url = "https://fleet.yango.com/api"
-
-    async def get_transactions(self, operation_id, from_date, to_date):
-        url = f"{self.base_url}/v1/reports/transactions/park/download-async?operation_id={operation_id}"
-        # url = "https://fleet.yango.com/api/v1/reports/transactions/park/download-async?operation_id=ba746b251a80468bb2c87bb87c607c96"
-        # url = "https://fleet.yango.com/api/v1/reports/transactions/park/download-async?operation_id=ba746b251a80468bb2c87bb87c607c95"
-        url = "https://fleet.yango.com/api/v1/reports/transactions/park/download-async?operation_id=ba746b251a80468bb2c87bb87c607c95"
-        # url = f"{self.base_url}/v1/reports/transactions/park/download-async"
-        
-        # Prepare the request body
-        payload = {
-            "query": {
-                "park": {
-                    "transaction": {
-                        "event_at": {
-                            "from": from_date,
-                            "to": to_date
-                        },
-                        "without_cash": True
-                    }
-                }
-            },
-            "charset": "utf-8-sig"
-        }
-        payload = '{"query":{"park":{"transaction":{"event_at":{"from":"2024-12-12T00:00:00.000+04:00","to":"2024-12-21T00:00:00.000+04:00"},"without_cash":true}}},"charset":"utf-8-sig"}'
-        payload = '{"query":{"park":{"transaction":{"event_at":{"from":"2024-12-12T00:00:00.000+04:00","to":"2024-12-21T00:00:00.000+04:00"},"without_cash":true}}},"charset":"utf-8-sig"}'
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            logging.info(f"{url}")
-            async with session.post(url, data=payload) as response:  # Note: use 'data' to send raw JSON
-                response.raise_for_status()
-                return await response.json()
-
-    async def get_payouts_list(self):
-        url = f"{self.base_url}/fleet/fleet-payouts-web/v2/payouts/list"
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(url) as response:
-                response.raise_for_status()
-                return await response.json()
-
-    async def get_report_status(self, operation_id):
-        url = f"{self.base_url}/fleet/reports-storage/v1/operations/status?operation_id={operation_id}"
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            logging.info(f"{url}")
-            async with session.get(url) as response:
-                response.raise_for_status()
-                return await response.json()
-
-    async def download_report(self, operation_id):
-        url = f"{self.base_url}/fleet/reports-storage/v1/operations/download?operation_id={operation_id}"
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            logging.info(f"{url}")
-            async with session.get(url) as response:
-                response.raise_for_status()
-                return await response.json()
-
-    async def get_transactions_list(self):
-        url = f"{self.base_url}/fleet/fleet-payouts-web/v1/transactions/list"
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            logging.info(f"{url}")
-            async with session.get(url) as response:
-                response.raise_for_status()
-                return await response.json()
-
-    async def get_order_details(self, order_id, driver_profile_id, tz="Asia/Dubai"):
-        encoded_tz = quote(tz)
-        url = f"{self.base_url}/fleet/fleet-orders/v1/orders/item/card?id={order_id}&tz={encoded_tz}&driver_profile_id={driver_profile_id}"
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            logging.info(f"{url}")
-            async with session.get(url) as response:
-                response.raise_for_status()
-                response_data = await response.json()
-
-                transactions_info = response_data.get("transactions_info", {})
-
-                flat_json = {}
-
-                for group in transactions_info.get("groups", []):
-                    group_name = group.get("name")
-                    for transaction in group.get("transactions", []):
-                        description = transaction.get("description")
-                        event_at = transaction.get("event_at")
-                        amount = transaction.get("amount")
-                        
-                        if description and event_at and amount:
-                            key = f"{group_name}: {description}"
-                            flat_json[key + "_amount"] = amount
-                            flat_json[key + "_event_at"] = event_at
-
-                return flat_json
-            
-
-
-            
 cookie_str = extract_cookie_value_yango()
 config = read_config("config.yml")
 client = YangoClient(cookie_str=cookie_str)
@@ -137,8 +34,8 @@ def save_csv_content(url, filename):
     return csv_content
 
 async def start_report_generation():
-    operation_id = "ba746b251a80468bb2c87bb87c607c96"
-    await client.get_transactions("some_operation_id", "2024-12-12T00:00:00.000+04:00", "2024-12-22T00:00:00.000+04:00")
+    operation_id = uuid.uuid4().hex
+    await client.get_transactions(operation_id, "2024-12-12T00:00:00.000+04:00", "2024-12-22T00:00:00.000+04:00")
     return operation_id
 
 async def wait_for_report_completion(operation_id):
@@ -152,18 +49,6 @@ async def wait_for_report_completion(operation_id):
 async def fetch_report_download_url(operation_id):
     return await client.download_report(operation_id)
 
-def convert_to_dict_list(csv_list):
-    columns = csv_list[0][0].split(';')
-    
-    result = []
-    for row in csv_list[1:]:
-        values = row[0].split(';')
-        # Combine the column names with the values to create a dictionary
-        row_dict = dict(zip(columns, values))
-        row_dict['Order ID'] = row_dict.get('Document', '')[7:]
-        result.append(row_dict)
-    
-    return result
 
 def download_and_parse_csv(download_url, filename):
     base_path = config['output_folder']
--- utils/yango_client.py
+++ utils/yango_client.py
@@ -4,115 +4,102 @@ from datetime import datetime
 from urllib.parse import quote
 
 class YangoClient:
-    def __init__(self, bearer_token):
+    def __init__(self, cookie_str):
         self.headers = {
-            "Authorization": f"Bearer {bearer_token}",
-            "Accept": "application/json, text/plain, */*",
+            "Cookie": cookie_str,
+            "Accept": "application/json",
+            "X-Client-Version": "fleet/13631",
+            "X-Park-Id": "c5cf968c4c2f4bbc947c8c0703b7e124",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "Language": "en"
         }
         self.base_url = "https://fleet.yango.com/api"
-        self.bearer_token = bearer_token
 
-    async def get_transactions(self, operation_id):
-        """
-        Fetches transactions report asynchronously.
-
-        Args:
-        - operation_id (str): The operation ID for the transaction report.
-
-        Returns:
-        - dict: JSON response containing transaction details.
-        """
+    async def get_transactions(self, operation_id, from_date, to_date):
         url = f"{self.base_url}/v1/reports/transactions/park/download-async?operation_id={operation_id}"
-        logging.info(f"Fetching transactions with operation_id={operation_id}")
-
+        # url = "https://fleet.yango.com/api/v1/reports/transactions/park/download-async?operation_id=ba746b251a80468bb2c87bb87c607c95"
+        
+        payload = {
+            "query": {
+                "park": {
+                    "transaction": {
+                        "event_at": {
+                            "from": from_date,
+                            "to": to_date
+                        },
+                        "without_cash": True
+                    }
+                }
+            },
+            "charset": "utf-8-sig"
+        }
+        
+        payload = '{"query":{"park":{"transaction":{"event_at":{"from":"2024-12-12T00:00:00.000+04:00","to":"2024-12-21T00:00:00.000+04:00"},"without_cash":true}}},"charset":"utf-8-sig"}'
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(url) as response:
+            logging.info(f"{url}")
+            async with session.post(url, data=payload) as response:  # Note: use 'data' to send raw JSON
+                logging.info(f"Response: {response.status} {response.reason} {url}")
                 response.raise_for_status()
                 return await response.json()
 
     async def get_payouts_list(self):
-        """
-        Fetches the list of payouts.
-
-        Returns:
-        - dict: JSON response containing payout details.
-        """
         url = f"{self.base_url}/fleet/fleet-payouts-web/v2/payouts/list"
-        logging.info("Fetching payouts list")
-
         async with aiohttp.ClientSession(headers=self.headers) as session:
             async with session.get(url) as response:
                 response.raise_for_status()
                 return await response.json()
 
     async def get_report_status(self, operation_id):
-        """
-        Checks the status of a report operation.
-
-        Args:
-        - operation_id (str): The operation ID for the report.
-
-        Returns:
-        - dict: JSON response containing the report status.
-        """
         url = f"{self.base_url}/fleet/reports-storage/v1/operations/status?operation_id={operation_id}"
-        logging.info(f"Checking report status for operation_id={operation_id}")
-
         async with aiohttp.ClientSession(headers=self.headers) as session:
+            logging.info(f"{url}")
             async with session.get(url) as response:
+                logging.info(f"Response: {response.status} {response.reason} {url}")
                 response.raise_for_status()
                 return await response.json()
 
     async def download_report(self, operation_id):
-        """
-        Downloads a report based on the operation ID.
-
-        Args:
-        - operation_id (str): The operation ID for the report.
-
-        Returns:
-        - dict: JSON response containing the download link and file name.
-        """
         url = f"{self.base_url}/fleet/reports-storage/v1/operations/download?operation_id={operation_id}"
-        logging.info(f"Downloading report for operation_id={operation_id}")
-
         async with aiohttp.ClientSession(headers=self.headers) as session:
+            logging.info(f"{url}")
             async with session.get(url) as response:
+                logging.info(f"Response: {response.status} {response.reason} {url}")
                 response.raise_for_status()
                 return await response.json()
 
     async def get_transactions_list(self):
-        """
-        Fetches a list of transactions.
-
-        Returns:
-        - dict: JSON response containing transaction details.
-        """
         url = f"{self.base_url}/fleet/fleet-payouts-web/v1/transactions/list"
-        logging.info("Fetching transactions list")
-
         async with aiohttp.ClientSession(headers=self.headers) as session:
+            logging.info(f"{url}")
             async with session.get(url) as response:
+                logging.info(f"Response: {response.status} {response.reason} {url}")
                 response.raise_for_status()
                 return await response.json()
 
     async def get_order_details(self, order_id, driver_profile_id, tz="Asia/Dubai"):
-        """
-        Fetches detailed information for a specific order.
-
-        Args:
-        - order_id (str): The order ID.
-        - driver_profile_id (str): The driver profile ID.
-        - tz (str): Timezone for the query (default: Asia/Dubai).
-
-        Returns:
-        - dict: JSON response containing order details.
-        """
         encoded_tz = quote(tz)
         url = f"{self.base_url}/fleet/fleet-orders/v1/orders/item/card?id={order_id}&tz={encoded_tz}&driver_profile_id={driver_profile_id}"
-        logging.info(f"Fetching order details for order_id={order_id}, driver_profile_id={driver_profile_id}")
-
         async with aiohttp.ClientSession(headers=self.headers) as session:
+            logging.info(f"{url}")
             async with session.get(url) as response:
+                logging.info(f"Response: {response.status} {response.reason} {url}")
                 response.raise_for_status()
-                return await response.json()
+                response_data = await response.json()
+
+                transactions_info = response_data.get("transactions_info", {})
+
+                flat_json = {}
+
+                for group in transactions_info.get("groups", []):
+                    group_name = group.get("name")
+                    for transaction in group.get("transactions", []):
+                        description = transaction.get("description")
+                        event_at = transaction.get("event_at")
+                        amount = transaction.get("amount")
+                        
+                        if description and event_at and amount:
+                            key = f"{group_name}: {description}"
+                            flat_json[key + "_amount"] = amount
+                            flat_json[key + "_event_at"] = event_at
+
+                return flat_json
